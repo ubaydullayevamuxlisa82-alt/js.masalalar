30402c736e87070d487497225220845924d24dd5
Muxlisa <ubaydullayevamuxlisa82@gmail.com>
2025-11-05 11:12:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// let age=20
+// const names="Hello world"
+// let a=true
+// let b
+// let c=null
+
+// console.log(age);
+// console.log(names);
+
+// console.log(typeof(age));
+// console.log(typeof(names));
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+// let arr=[1,2,3,4,5,6,7]
+// console.log(arr);
+
+// function ayirma(a, b){
+//     return a-b
+// }
+
+// const person = {
+//     name : "Ali",
+//     age : 20,
+// }
+// console.log (typeof(person));
+
+// alert("Hello world")
+// const isSucces= confirm("Are you 18+?")
+// console.log(isSucces);
+
+// const name1 = prompt('Write your name!!!')
+// console.log(name1);
+// const age = +prompt('Write your age!!!')
+// console.log(age);
+
+// let rang = "qora"
+// console.log(rang);
+// rang="qizil"
+// console.log(rang);
+
+let son= 6;
+son++;
+console.log(son);
